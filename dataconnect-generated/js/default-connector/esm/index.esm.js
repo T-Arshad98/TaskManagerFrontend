65014cd1dcad818a47c6dadc592c5a9c944b8406
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-05 15:58:08 -0700

    restart

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'taskmanagerfrontend',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'taskmanagerfrontend',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
