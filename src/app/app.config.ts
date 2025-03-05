8f9b36fd7fdfb817de4b4564a1f0eceb269f0a35
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-05 12:56:10 -0700

    app.comp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,12 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideHttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from './environments/environment';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideClientHydration(withEventReplay()), 
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
   ]
 };
