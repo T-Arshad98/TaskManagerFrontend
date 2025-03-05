34de061d2eb9b3c536f45bc54603ba164bdeeab3
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-05 14:28:10 -0700

    app.comp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,22 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideClientHydration(withEventReplay()), 
     provideHttpClient(),
-    provideFirebaseApp(() => {
-      if (isPlatformBrowser(inject(PLATFORM_ID))) {
-        return initializeApp(environment.firebaseConfig);
-      }
-      // Optional, since it's null in dev-mode and SSG
-      const request = inject(REQUEST, { optional: true });
-      const authIdToken = request?.headers.get('authorization')?.split("Bearer ")[1];
-      return initializeServerApp(environment.firebaseConfig, {
-        authIdToken,
-        releaseOnDeref: request || undefined
-      });
-    }),
-    provideAuth(() => getAuth(inject(FirebaseApp))), provideFirebaseApp(() => initializeApp({ projectId: "taskmanager-e27ea", appId: "1:404785636178:web:92167d92ab877f9975108b", storageBucket: "taskmanager-e27ea.firebasestorage.app", apiKey: "AIzaSyBTzwIYp3-Qs_EAj8j9zikuu01XA5vvme4", authDomain: "taskmanager-e27ea.firebaseapp.com", messagingSenderId: "404785636178", measurementId: "G-16BS8BQTVE" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
-  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-}), provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
   ]
 };
