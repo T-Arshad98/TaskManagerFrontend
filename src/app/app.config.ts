43c0d25e50fa330d7d48eb4d5c117ddae805b50d
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-05 12:06:05 -0700

    add phc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideClientHydration(withEventReplay()), 
+    provideHttpClient(),
+  ]
 };
