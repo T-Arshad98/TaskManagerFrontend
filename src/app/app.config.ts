7cd131b2d5f44dacecb748b746035a3e227ab1cb
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-05 12:03:50 -0700

    revert config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideClientHydration(withEventReplay()), 
-    provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideHttpClient(),
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(withEventReplay())]
 };
