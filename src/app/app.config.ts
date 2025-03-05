5829c97d71822a9a08aeafe8a8726240f6df6ca3
Taimoor Arshad <taimoor.1998@gmail.com>
2025-03-05 15:17:12 -0700

    app.comp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,27 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { isPlatformBrowser } from '@angular/common';
+import { initializeServerApp } from '@angular/fire/app';
+import { FirebaseApp } from '@angular/fire/app';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideClientHydration(withEventReplay()), 
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    provideFirebaseApp(() => {
+      if (isPlatformBrowser(inject(PLATFORM_ID))) {
+        return initializeApp(environment.firebaseConfig);
+      }
+      // Optional, since it's null in dev-mode and SSG
+      const request = inject(REQUEST, { optional: true });
+      const authIdToken = request?.headers.get('authorization')?.split("Bearer ")[1];
+      return initializeServerApp(environment.firebaseConfig, {
+        authIdToken,
+        releaseOnDeref: request || undefined
+      });
+    }),
+    provideAuth(() => getAuth(inject(FirebaseApp))),
   ]
 };
